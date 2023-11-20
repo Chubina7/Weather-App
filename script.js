ea7dad469295552c2fe42c0bf4348a8f5047fd2d
Chubina <120401101+Chubina7@users.noreply.github.com>
2023-11-20 15:10:34 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// DOM Elements
+//                                      ** DOM Elements **
+
 const currentTemp = document.getElementById('temperature')
 const weatherStatus = document.getElementById('weatherStatus')
 const mainweatherIcon = document.getElementById('mainweatherIcon')
@@ -10,8 +11,15 @@ const sunset = document.getElementById('sunset')
 const sunrise = document.getElementById('sunrise')
 const sunriseItem = document.getElementById('sunriseItem')
 const sunsetItem = document.getElementById('sunsetItem')
+const overlay = document.querySelector('.overlay')
+const overlayCard = document.querySelector('.overlay-card')
+const hourlyItemDivArr = Array.from(document.querySelectorAll('.hourly-item'))
+const hourlyTimeArr = Array.from(document.querySelectorAll('.hourly-item-time'))
+const hourlyTempArr = Array.from(document.querySelectorAll('.hourly-item-temp'))
+const hourlyItemImgArr = Array.from(document.querySelectorAll('.hourly-item-img'))
 
-//                                      Current Weather
+//                                      ** Current Weather **
+
 var userCity // Variable to save the name of the city where the user is loggined in
 navigator.geolocation.getCurrentPosition(pos => { // Getting location details in numbers
     // Making Binding to save the user's longitude and lattitude numbers
@@ -47,13 +55,52 @@ navigator.geolocation.getCurrentPosition(pos => { // Getting location details in
                     sunsetItem.style.display = 'none'
                     sunriseItem.style.display = 'flex'
                 }
-
-                console.log(data);
             })
-}, error => console.log("Here should be popup"))
+}, () => {
+    overlay.style.display = 'block'
+    overlayCard.style.display = 'block'
+    console.error('User dennied location tracking, so the weather is unavailable!');
+})
 
-//                                      Current Date
+//                                      ** Current Date **
+
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 currentDate.innerHTML = `${weekDays[new Date().getDay()]}, ${new Date().getDate()} ${months[new Date().getMonth()]}`
 
+//                                      ** Hourly Date **
+
+navigator.geolocation.getCurrentPosition(pos => { // Getting location details in numbers
+    // Making Binding to save the user's longitude and lattitude numbers
+    const usersLongitude = pos.coords.longitude
+    const usersLattitude = pos.coords.latitude
+
+    // Fetching data to get hourly information
+    fetch(`https://api.open-meteo.com/v1/dwd-icon?latitude=${usersLattitude}&longitude=${usersLongitude}&hourly=temperature_2m,relative_humidity_2m,precipitation,rain&timezone=auto&forecast_days=1`)
+        .then(response => response.json())
+        .then(result => {
+            // Puting hours into DOM elements
+            hourlyTimeArr.forEach(hourlyTime => {
+                hourlyTime.innerHTML = result.hourly.time[hourlyTimeArr.indexOf(hourlyTime)].split('T')[1]
+                // Change style the element which is current time
+                if (Math.floor(hourlyTime.innerHTML.split(':')[0] * 1) == new Date().getHours()) {
+                    hourlyItemDivArr[hourlyTimeArr.indexOf(hourlyTime)].classList.add('hourly-item-active')
+                }
+            })
+
+            // Putting temperature in each time item
+            hourlyTempArr.forEach(hourlyTemp => {
+                hourlyTemp.innerHTML = '≈ ' + Math.round(result.hourly.temperature_2m[hourlyTempArr.indexOf(hourlyTemp)]) + '°'
+            })
+
+            // Putting img in each hourly item
+            hourlyItemImgArr.forEach(hourlyImg => {
+                hourlyImg.src = './src/svg/01d.svg'
+                // Setting images appropriately to the day or night
+                if (Math.floor(hourlyTimeArr[hourlyItemImgArr.indexOf(hourlyImg)].innerHTML.split(':')[0] * 1) >= sunset.innerHTML.split(':')[0] * 1
+                    || Math.floor(hourlyTimeArr[hourlyItemImgArr.indexOf(hourlyImg)].innerHTML.split(':')[0] * 1) <= sunrise.innerHTML.split(':')[0] * 1) {
+                    hourlyImg.src = './src/svg/01n.svg'
+                }
+            })
+        })
+})
